e39160c69bf7035a365d0c5b10c5c0adfb203eff
zyh-test <1014608521@qq.com>
2021-06-07 10:34:56 +0800

    first commit

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,10 +1,10 @@
 class TestLogin:
 
     def test_a(self):
-        assert 0
+        assert 1
 
     def test_b(self):
         assert 1
 
     def test_c(self):
-        assert 0
+        assert 1
